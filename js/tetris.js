dc16a2c069bd32a3cb1c10b8a88c9a1922e17926
hxsherry <1053288784@qq.com>
2017-08-31 00:15:00 +0800

    Ready to change the body touch event.

--- js/tetris.js
+++ js/tetris.js
@@ -1,8 +1,11 @@
 function random(a, b) {
     return a + Math.floor(Math.random() * (b - a + 1));
 }
-WIDTH_NUM = 13;
-HEIGH_NUM = 20;
+WIDTH_NUM = 12;
+HEIGH_NUM = 22;
+UNIT = 24;
+soundInstance = null;
+
 
 function Category() {
 
@@ -83,11 +86,11 @@ Type.ALL[Type.T] = [0x04e0, 0x4640, 0x0720, 0x0262,
     [-5, WIDTH_NUM - 3, HEIGH_NUM - 4, -1]
 ];
 
-
 function Grid() {
     this.x = 0;
     this.y = 0;
-    this.unit = 20;
+    this.unit = UNIT;
+    this.unit1 = 13;
     this.$dom = $('<span></span>');
     this.category = null;
     this.color = 0;
@@ -146,8 +149,6 @@ Shape.prototype.getGrids = function (previewGrids) {
     return grids;
 
 };
-
-
 Shape.prototype.setCategory = function (state) {
     var grids = this.getGrids();
     for (var i = 0; i < grids.length; i++) {
@@ -243,21 +244,21 @@ Shape.prototype.dropOneStep = function () {
 function Game() {
     this.$stage = $('.stage');
     this.grids = [];
-
     this.$box = $('.box');
     this.boxGrids = [];
-
     this.shape = null;
     this.previewShape = null;
-
-    this.interval = 200;
+    this.interval = 500;
     this.score = 0;
     this.highScore = 0;
     this.intervalHandler = null;
-    this.bgSound = null;
-    this.eliSound = null;
+    this.dropPaused = false;
+    this.keypress = true;
+    this.touch = true;
+
 
 }
+
 Game.prototype.fillGrids = function () {
     for (var x = 0; x < WIDTH_NUM; x++) {
         var xArr = [];
@@ -286,8 +287,8 @@ Game.prototype.fillGrids = function () {
 
             grid1.$dom.css(
                 {
-                    left: x1 * grid1.unit + "px",
-                    top: y1 * grid1.unit + 'px'
+                    left: x1 * grid1.unit1 + "px",
+                    top: y1 * grid1.unit1 + 'px'
                 });
 
             this.$box.append(grid1.$dom);
@@ -297,14 +298,19 @@ Game.prototype.fillGrids = function () {
     }
 
 };
-Game.prototype.listenEvent = function () {
+Game.prototype.bindKeyEvent = function () {
     var that = this;
-    $('body').keydown(function (e) {
+    var $body = $('body');
+    $body.keydown(function (e) {
+
+        if (!that.keypress) {
+            return;
+        }
         if (e.keyCode == 38) {
             that.shape.changeDirection();
+            createjs.Sound.play('change', {volume: 0.3});
 
         } else if (e.keyCode == 39) {
-
             that.shape.moveRightOneStep();
         } else if (e.keyCode == 40) {
             that.interval = 0;
@@ -314,22 +320,172 @@ Game.prototype.listenEvent = function () {
         }
 
     });
-    $('.reStart').click(function () {
-        that.start();
-    });
-    $('.up').click(function () {
-        that.shape.changeDirection();
-    });
-    $('.right').click(function () {
-        that.shape.moveRightOneStep();
-    });
-    $('.down').click(function () {
-        that.interval = 0;
-    });
-    $('.left').click(function () {
-        that.shape.moveLeftOneStep();
-    })
 
+};
+Game.prototype.bindTouchEvent = function () {
+    var that = this;
+    var $reStart = $('.reStart');
+    var $btnDanger = $('.btn-danger');
+    var $guidePause = $('.guide-pause');
+    var $guideHome = $('.guide-home');
+    $reStart.on('touchstart', function (e) {
+        e.preventDefault();
+        that.dropPaused = false;
+        that.keypress = true;
+        that.touch = true;
+        that.start();
+        $reStart.addClass('bgColor');
+    });
+    $reStart.on('touchend', function (e) {
+        e.stopPropagation();
+        $reStart.removeClass('bgColor');
+    });
+    $btnDanger.on('touchstart', function (e) {
+        e.stopPropagation();
+        $('.container').show();
+        $('.cover').hide();
+        soundInstance = createjs.Sound.play("tetris", {loop: -1, volume: 0.3});
+        that.dropPaused = false;
+        that.keypress = true;
+        that.touch = true;
+        that.start();
+
+    });
+    $guideHome.on('touchstart', function (e) {
+        e.stopPropagation();
+        $('.cover').show();
+        $('.container').hide();
+        that.dropPaused = true;
+        that.keypress = false;
+        that.touch = false;
+        soundInstance.stop();
+    });
+    $guidePause.on('touchstart', function (e) {
+        e.stopPropagation();
+        var $pause = $('.guide-pause');
+        if ($pause.html() === '暂停') {
+            $pause.html('继续');
+            $pause.css({'background': 'red'});
+            that.dropPaused = true;
+            that.keypress = false;
+            that.touch = false;
+            soundInstance.setPaused(true);
+        } else {
+            $pause.html('暂停');
+            $pause.css({'background': '#bbb'});
+            that.dropPaused = false;
+            that.keypress = true;
+            that.touch = true;
+            soundInstance.play();
+        }
+    });
+
+
+    var startX = 0;
+    var startY = 0;
+    var moveX = 0;
+    var moveY = 0;
+    var lastStep = 0;
+    var startTime = null;
+    var isFastDrop = false;
+    var isMoving = false;
+    var $body = $('body');
+    $body.on('touchstart', function (e) {
+        if (!that.touch) {
+            return;
+        }
+        var x1 = e.touches[0].clientX;
+        var y1 = e.touches[0].clientY;
+        startX = x1;
+        startY = y1;
+
+        moveX = 0;
+        moveY = 0;
+
+        lastStep = 0;
+        startTime = new Date().getTime();
+        isFastDrop = false;
+        isMoving = false;
+    });
+    $body.on('touchmove', function (e) {
+        if (!that.touch) {
+            return;
+        }
+        var x2 = e.touches[0].clientX;
+        var y2 = e.touches[0].clientY;
+        moveX = x2;
+        moveY = y2;
+
+        var X = Math.abs(moveX - startX);
+        var Y = Math.abs(moveY - startY);
+
+        //左右移动
+        if (X - Y > 0) {
+            var step = Math.floor((moveX - startX) / UNIT);
+            if (step < 0) {
+                step++;
+            }
+            var i = 0;
+            var deltaStep = step - lastStep;
+            if (deltaStep > 0) {
+                for (i = 0; i < deltaStep; i++) {
+                    that.shape.moveRightOneStep();
+                    isMoving = true;
+                }
+
+            } else {
+                for (i = 0; i < -deltaStep; i++) {
+                    that.shape.moveLeftOneStep();
+                    isMoving = true;
+                }
+            }
+            lastStep = step;
+
+        }
+        else {
+            if (!isMoving && !isFastDrop) {
+                var currentTime = new Date().getTime();
+                if ((moveY - startY) > UNIT && (currentTime - startTime) < 100) {
+                    that.interval = 0;
+                    isFastDrop = true;
+                } else if ((moveY - startY) > UNIT && (currentTime - startTime) > 200) {
+                    that.interval = 100;
+                    isFastDrop = true;
+
+                }
+
+            }
+
+
+        }
+
+
+    });
+    $body.on('touchend', function (e) {
+        if (!that.touch) {
+            return;
+        }
+        var currentTime = new Date().getTime();
+        if (!isMoving) {
+            if ((moveY - startY) > UNIT && (currentTime - startTime) < 100) {
+                that.interval = 0;
+                isFastDrop = true;
+            } else if ((moveY - startY) > UNIT && (currentTime - startTime) > 200) {
+                that.interval = 500;
+                isFastDrop = false;
+
+            }
+
+        }
+        if (!isMoving && !isFastDrop) {
+            if (currentTime - startTime < 200) {
+                that.shape.changeDirection();
+                createjs.Sound.play('change', {volume: 0.3});
+            }
+        }
+
+
+    });
 
 };
 Game.prototype.refreshStage = function () {
@@ -358,9 +514,7 @@ Game.prototype.eliminateOneLine = function (y) {
                 this.grids[x2][y2].category = Category.GROUND;
 
             } else {
-
                 this.grids [x2][y2].category = this.grids[x2][y2 - 1].category;
-                this.eliSound.play();
             }
         }
 
@@ -372,6 +526,7 @@ Game.prototype.eliminateLines = function () {
     for (var y = HEIGH_NUM - 1; y >= 0; y--) {
         while (this.eliminateOneLine(y)) {
             lines++;
+            createjs.Sound.play("elimin", {volume: 1});
         }
     }
     if (lines == 1) {
@@ -399,30 +554,27 @@ Game.prototype.isGameOver = function () {
     return grids.length < 4;
 
 };
-Game.prototype.initBgMusic = function () {
-    this.bgSound = new Howl({
-        src: ['sound/tetris.mp3'],
-        autoplay: false,
-        loop: true,
-        volume: 0.3
-    });
-    this.bgSound.once('load', function () {
-        this.play();
-    });
+Game.prototype.preLoadSrc = function () {
+    var $stastus = $('#status');
+    $stastus.show();
+    $('.btn-danger').hide();
+    var manifest = [
+        {id: 'tetris', src: 'sound/tetris.mp3'},
+        {id: 'elimin', src: 'sound/elimin.mp3'},
+        {id: 'cover', src: 'img/cover.jpg'},
+        {id: 'change', src: 'sound/change.mp3'}
+    ];
+    var loader = new createjs.LoadQueue();
+    createjs.Sound.alternateExtensions = ['mp3'];
+    loader.installPlugin(createjs.Sound);
+    loader.loadManifest(manifest);
+    loader.on('complete', function () {
+        $stastus.hide();
+        $('.btn-danger').show();
+    })
 
 
 };
-Game.prototype.initEliminSound = function () {
-    this.eliSound = new Howl({
-        src: ['sound/elimin.mp3'],
-        autoplay: false,
-        loop: false,
-        volume: 0.3
-    });
-
-
-};
-
 Game.prototype.refreshBox = function () {
     if (!this.previewShape) {
         this.previewShape = new Shape(this);
@@ -444,7 +596,6 @@ Game.prototype.refreshBox = function () {
     }
 
 };
-
 Game.prototype.refreshShape = function () {
     if (!this.shape) {
         this.shape = new Shape(this);
@@ -456,7 +607,6 @@ Game.prototype.refreshShape = function () {
     this.shape.color = random(1, 5);
 
 };
-
 Game.prototype.broomStage = function () {
     for (var x = 0; x < WIDTH_NUM; x++) {
         for (var y = 0; y < HEIGH_NUM; y++) {
@@ -467,15 +617,22 @@ Game.prototype.broomStage = function () {
     }
 
 };
-
 Game.prototype.start = function () {
     var that = this;
+    soundInstance.stop();
+    soundInstance.play();
+    var $pause = $('.guide-pause');
+    if ($pause.html() === '继续') {
+        $pause.html('暂停');
+        $pause.css({'background': '#bbb'});
+        that.dropPaused = false;
+        that.keypress = true;
+        that.touch = true;
+        soundInstance.play();
+    }
     if (that.intervalHandler) {
         clearInterval(that.intervalHandler);
     }
-    this.bgSound.stop();
-    this.bgSound.play();
-
     this.refreshBox();
     this.refreshShape();
     this.score = 0;
@@ -485,19 +642,21 @@ Game.prototype.start = function () {
     var temp = 0;
 
     function intervalFunc() {
+        if (that.dropPaused) {
+            return;
+        }
         if (temp > that.interval) {
             temp = 0;
             var canDrop = that.shape.dropOneStep();
             if (!canDrop) {
                 if (that.isGameOver()) {
-                    that.bgSound.stop();
+                    soundInstance.stop();
                     alert('game is over!');
                     clearInterval(that.intervalHandler);
 
-
                 } else {
                     that.eliminateLines();
-                    that.interval = 200;
+                    that.interval = 500;
                     that.refreshShape();
                     that.refreshBox();
                 }
@@ -510,19 +669,20 @@ Game.prototype.start = function () {
 
     this.intervalHandler = setInterval(intervalFunc, 10);
 };
-
-
 Game.prototype.init = function () {
     this.fillGrids();
-    this.initBgMusic();
-    this.initEliminSound();
-    this.listenEvent();
-    this.start();
+    this.preLoadSrc();
+    this.bindKeyEvent();
+    this.bindTouchEvent();
 };
+
+
 
 $(document).ready(function () {
     var game = new Game();
     game.init();
+
 });
+
 
 
